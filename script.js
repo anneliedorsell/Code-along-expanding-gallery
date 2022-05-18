5de71a2121cc92693dc9febf6a620d994f6cd7a5
Annelie <annelie.dorsell@gmail.com>
2022-05-18 22:48:50 +0200

    added javascript

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const panels = document.querySelectorAll(".panel")
+// querySelectorAll = select hat you want, id, class...
 
-console.log(panels);
-// a selection of html (HTMLCollection)
+//console.log(panels);
+//console.log(panels[0]); //The first index(pic)
+//Nodelist in the console means a selection of html (HTMLCollection)
+
+panels.forEach(panel => {
+    // add an eventlist to listen to the click
+    panel.addEventListener("click", () => {
+        panel.classList.add("active")
+        removeActiveClass();
+
+        panel.classList.add("active");
+    });
+
+});
+
+function removeActiveClass() {
+    panels.forEach((panel) => {
+        //remove all active panel classes
+        panel.classList.remove("active");
+
+    });
+}
